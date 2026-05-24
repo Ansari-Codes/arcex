175dd1c12fc329e1a556ebfa02825472b6745024
Ansari-Codes <ansaricodes@gmail.com>
2026-05-24 12:30:26 +0500

    .

--- Arcex/components/__init__.py
+++ Arcex/components/__init__.py
@@ -1,5 +1,5 @@
 from Arcex.components.Buttons import Button
 from Arcex.components.Inputs import Input, Select, Date, Textarea, Checkbox, Number, Radio
 from Arcex.components.Text import Text
-from Arcex.components.Layouts import Div, LBreak, LHl, LVl, Col, Row
+from Arcex.components.Layouts import Div, LBreak, LHl, LVl, Col, Row, Card
 from Arcex.ax.Elements import Element
